f3e1b336ed2d15dbe9e0152d6b4ff2ee6ec20682
galenomoon <galeno.dev@gmail.com>
2025-10-04 17:27:06 -0300

    Atualiza o esquema de plano para alterar o tipo do campo 'price' de string para number, garantindo que o preço seja um valor numérico e obrigatório.

--- src/app/(authenticated)/planos/schemas/plan-schema.tsx
+++ src/app/(authenticated)/planos/schemas/plan-schema.tsx
@@ -5,7 +5,7 @@ export const planSchema = z.object({
   weeklyClasses: z.number().min(1, "Deve ser pelo menos 1 aula por semana"),
   durationInDays: z.number().min(1, "Duração deve ser pelo menos 1 dia"),
   isSecondary: z.boolean(),
-  price: z.string().min(1, "Preço é obrigatório")
+  price: z.number().min(1, "Preço é obrigatório")
 });
 
 export type PlanSchema = z.infer<typeof planSchema>;
